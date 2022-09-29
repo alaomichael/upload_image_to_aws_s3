0e7ff40677e986b89375524ee4db9b225d214146
Michael <contactleomax@gmail.com>
2022-09-29 11:01:13 +0100

    multiple file upload

--- index.js
+++ index.js
@@ -3,9 +3,16 @@ const multer = require('multer');
 const app = express();
 
 const upload = multer({dest:"uploads/"});
-app.post('/upload',upload.single("file"), function(req, res) {  
+// single file upload
+// app.post('/upload',upload.single("file"), function(req, res) {
+// res.json({status: 'success'});
+// });
+
+// multiple file uploads, with max number of upload
+app.post('/upload',upload.array("file", 4), function(req, res) {
 res.json({status: 'success'});
 });
+
 
 const PORT = 4000;
 app.listen(PORT, () => console.log(`listening on port ${PORT}`));
