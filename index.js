facd4cbda0ba071596931c1660a0e35b7b0e6c2d
Michael <contactleomax@gmail.com>
2022-09-29 12:11:52 +0100

    filefilter,limits, errorHandling

--- index.js
+++ index.js
@@ -38,21 +38,46 @@ const storage = multer.diskStorage({
 });
 
 const fileFilter = (req, file, cb) => {
-    if(file.mimetype.split('/')[0] === 'image'){
+    if (file.mimetype.split('/')[0] === 'image') {
         // allow to pass
         cb(null, true);
-    } else{
+    } else {
         // throw exception  error and don't allow to pass    
-        cb(new Error("file is not of the correct type: " + file.mimetype),false);
+        cb(new multer.MulterError("LIMIT_UNEXPECTED_FILE"), false);
     }
 };
 
-const uploadCustom = multer({ storage, fileFilter }); //middleware name
+//middleware name, 
+const uploadCustom = multer({
+    storage,
+    fileFilter,
+    limits: { fileSize: 10000000, files: 2 }, // limit file size and max number of files allowed for upload
+});
 
+// field name = "file";
 app.post('/upload', uploadCustom.array("file"), (req, res) => {
     res.json({ status: 'success' });
 });
 
+app.use((error, req, res, next) => {
+    if (error instanceof multer.MulterError) {
+        if (error.code === "LIMIT_FILE_SIZE") {
+            return res.status(400).json({
+                message: "file is too large",
+            });
+        }
+        if (error.code === "LIMIT_FILE_COUNT") {
+            return res.status(400).json({
+                message: "file limit reached",
+            });
+        }
+        if (error.code === "LIMIT_UNEXPECTED_FILE") {
+            return res.status(400).json({
+                message: "file must be an image",
+            });
+        }
+    }
+});
 
 const PORT = 4000;
 app.listen(PORT, () => console.log(`listening on port ${PORT}`));
