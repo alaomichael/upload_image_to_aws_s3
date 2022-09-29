7d26aa18c1329ac0b80b55b03daf2f13bdc4e4e9
Michael <contactleomax@gmail.com>
2022-09-29 11:31:26 +0100

    custom file name

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const multer = require('multer');
+const uuid = require('uuid').v4;
 const app = express();
 
-const upload = multer({ dest: "uploads/" });
+// const upload = multer({ dest: "uploads/" });
+
 // single file upload
 // app.post('/upload',upload.single("file"), function(req, res) {
 // res.json({status: 'success'});
@@ -14,14 +16,33 @@ const upload = multer({ dest: "uploads/" });
 // });
 
 // allows multiple files to be uploaded with different field name
-const multiUpload = upload.fields([
-    { name: 'avatar', maxCount: 1 },
-    { name: 'resume', maxCount: 1 },
-]);
+// const multiUpload = upload.fields([
+//     { name: 'avatar', maxCount: 1 },
+//     { name: 'resume', maxCount: 1 },
+// ]);
 
-app.post('/upload', multiUpload, (req, res) => {
+// app.post('/upload', multiUpload, (req, res) => {
+//     console.log(req.files);
+//     res.json({ status: 'success' });
+// });
+
+// custom file name
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "uploads");
+    },
+    filename: (req, file, cb) => {
+        const { originalname } = file;
+        cb(null, `${uuid()}-${originalname}`);
+    },
+});
+
+const uploadCustom = multer({ storage })
+
+app.post('/upload', uploadCustom.array("file"), (req, res) => {
     res.json({ status: 'success' });
 });
+
 
 const PORT = 4000;
 app.listen(PORT, () => console.log(`listening on port ${PORT}`));
